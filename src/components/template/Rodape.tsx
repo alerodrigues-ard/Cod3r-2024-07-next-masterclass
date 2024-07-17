6a0f50926144687aba14b6b8b5527a5773568412
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2024-07-16 21:41:29 -0300

    Páginas e componentes

---
+++ src/components/template/Rodape.tsx
@@ -0,0 +1,15 @@
+﻿import { IconCopyright, IconHeart } from "@tabler/icons-react"
+
+export default function Rodape() {
+  return (
+    <footer>
+      <div className="flex justify-end  items-center gap-2 bg-zinc-700 p-4 text-zinc-400 text-sm">
+        <span>
+          Desenvolvido por <strong>Alexandre</strong>
+        </span>
+        <IconCopyright />
+        <span>{new Date().getFullYear()}</span>
+      </div>
+    </footer>
+  )
+}
